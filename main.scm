b24ebd41d39a43267b6eceb299dace11c2be21d9
Talun523 <271750489+itaru-maker@users.noreply.github.com>
2026-08-22 21:44:04 +0900

    add the "set" function

--- main.scm
+++ main.scm
@@ -20,10 +20,10 @@
 
 (interp-run mylang "
 . write \"ミクーッ！！ミクーッ！！ヒャー！！おわっほあああああああああああああああああああああああああああああああああ！！\"  ;
-. write '; ;
-. write 123 ; ;
-. let 'hello! \"hello world\" ;
-. write hello! ;
+. set 'hello 'kani ;
+. let 'hello \"hello world\" ;
+. set 'hello \"goodbye world\" ;
+. write hello ;
 ")
 	
 
