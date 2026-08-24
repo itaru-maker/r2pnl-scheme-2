0a5d886bf423bb3476cbdce95a07bd516653afb8
Talun523 <271750489+itaru-maker@users.noreply.github.com>
2026-08-24 16:41:34 +0900

    clean files

--- main.scm
+++ main.scm
@@ -1,4 +1,12 @@
-;(set! %load-extensions (cons ".sld" %load-extensions));guileの人は書く
+;;(set! %load-extensions (cons ".sld" %load-extensions));guileの人は書く
+#|===実行方法===
+gauche
+gosh -r7 -I. main.scm
+chibi
+chibi-scheme main.scm
+kawa
+kawa -Dkawa.import.path="./*.sld" --r7rs main.scm
+|#
 
 (import (scheme base)
 	(scheme write)
@@ -7,6 +15,7 @@
 	(mylang env)
 	(mylang lexar)
 	(mylang parser)
+	(mylang error)
 	(mylang builtin database)
 	(mylang interpreter)
 	(mylang builtin math-func)
@@ -17,10 +26,13 @@
 
 
 
+
 (interp-run mylang "
 . write \"ミクーッ！！ミクーッ！！ヒャー！！おわっほあああああああああああああああああああああああああああああああああ！！\"  ;
 . write write ;
+. write \" ;
 
 ")
+
 
 
